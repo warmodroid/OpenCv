493bc4b80455e2960cd4bcc4a13438cb17bf79d2
warmodroid <supermohit@yahoo.in>
2016-04-07 21:29:32 +0530

    How to read and write image.

--- Image/Image/Image.py
+++ Image/Image/Image.py
@@ -4,7 +4,14 @@ import cv2
 
 #Load image from folder
 
+
+cv2.namedWindow('Rope',cv2.WINDOW_NORMAL)   #To resize the Window.
 img = cv2.imread('i.jpg',1)
+
 cv2.imshow('i',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+k=cv2.waitKey(0)  #It waits indefinitely for a key stroke
+if k ==27:
+    cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('new.jpg',img)
+    cv2.destroyAllWindows()
